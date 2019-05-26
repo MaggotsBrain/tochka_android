a4847723892d7c275d2ed5e62fc6c381f266c2d7
Mikhail Stepanov <mikhail.stepanovv@yandex.ru>
2019-05-26 18:30:50 +0300

    anal karnaval

--- app/src/main/java/social/tochka/android/main/ConstructFragment.java
+++ app/src/main/java/social/tochka/android/main/ConstructFragment.java
@@ -27,7 +27,6 @@ public class ConstructFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_construct_splash, container, false);
     }
